da6146d3d969a663946d14248516a2ba51b0d3d4
jovanlekic12 <lekicjovan1212@gmail.com>
2024-10-03 17:53:06 +0200

    mount application

---
+++ src/data.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+    name: "Gear Head",
+    img: "./images/gearHead.png",
+  },
+  {
+    name: "Squanchy",
+    img: "./images/squanchy.png",
+  },
+  {
+    name: "Snuffles",
+    img: "./images/snuffles.png",
+  },
+  {
+    name: "Krombopulos Michael",
+    img: "./images/michael",
+  },
+  {
+    name: "Coach Feratu",
+    img: "./images/vampire",
+  },
+  {
+    name: "Bird Person",
+    img: "./images/birdPerson.png",
+  },
+  {
+    name: "Prince Nebulon",
+    img: "./images/princeNebulon",
+  },
+  {
+    name: "Scary Terry",
+    img: "./images/Terry.png",
+  },
+  {
+    name: "Doofus Rick",
+    img: "./images/J19ick.png",
+  },
+  {
+    name: "President Curtis",
+    img: "./images/president.png",
+  },
+  {
+    name: "Abrodolph Lincoler",
+    img: "./images/abraham",
+  },
+  {
+    name: "Rick Sancez",
+    img: "./images/rickkk.png",
+  },
+];
